7a5fb9707288c7a47897ece85e640163fcf04e67
Andronik Nazaryan <anrinazaryan@gmail.com>
2021-11-17 22:52:03 +0400

    init

---
+++ background.js
@@ -0,0 +1,17 @@
+﻿function blockRequest() {
+  return { cancel: true }
+}
+
+function updateFilters(urls) {
+  if (chrome.webRequest.onBeforeRequest.hasListener(blockRequest)) {
+    chrome.webRequest.onBeforeRequest.removeListener(blockRequest)
+  }
+  chrome.webRequest.onBeforeRequest.addListener(blockRequest, { urls: urls }, ['blocking'])
+}
+
+fetch('https://steamboost.ge/ads.json')
+  .then(r => r.json())
+  .then(response => {
+    console.log('IT WORKS')
+    updateFilters(response.ads)
+  })
